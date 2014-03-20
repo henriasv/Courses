42973bb2e972e8a9244fd923ab6e34a05aa991d9
Henrik Sveinsson <henrik.sveinsson@me.com>
2014-03-20 10:54:07 +0100

    m

---
+++ inf3380/oblig1/oblig1/serial/runserial.py
@@ -0,0 +1,4 @@
+import subprocess
+
+#subprocess.call(['make'])
+subprocess.call(['./serial_main', '0.1', '3', 'mona_lisa_noisy.jpg', '/Users/henriksveinsson/git_repos/Courses/inf3380/oblig1/mona_lisa_noisy.jpg', '/Users/henriksveinsson/git_repos/Courses/inf3380/oblig1/mona_lisa_noisy_denoised.jpg'])
