4ce4322f9b87f5103aa98f3c14f930ed1cfecde1
Henrik Andersen Sveinsson <henrik.sveinsson@me.com>
2014-03-20 15:25:28 +0100

    serial implementation works

--- inf3380/oblig1/oblig1/serial/runserial.py
+++ inf3380/oblig1/oblig1/serial/runserial.py
@@ -1,4 +1,3 @@
 import subprocess
 
-#subprocess.call(['make'])
-subprocess.call(['./serial_main', '0.1', '3', 'mona_lisa_noisy.jpg', '/Users/henriksveinsson/git_repos/Courses/inf3380/oblig1/mona_lisa_noisy.jpg', '/Users/henriksveinsson/git_repos/Courses/inf3380/oblig1/mona_lisa_noisy_denoised.jpg'])
+subprocess.call(['./serial_main', '0.1', '100', '/home/henrik/git_repos/Courses/inf3380/oblig1/mona_lisa_noisy.jpg', '/home/henrik/git_repos/Courses/inf3380/oblig1/mona_lisa_noisy_denoised.jpg'])
--- inf3380/oblig1/oblig1/serial/serial_main.c
+++ inf3380/oblig1/oblig1/serial/serial_main.c
@@ -19,6 +19,11 @@ typedef struct
   int n;               /* # pixels in y-direction */
 } image;
 
+void allocate_image(image *u, int m, int n);
+void deallocate_image(image *u);
+void convert_jpeg_to_image(const unsigned char* image_chars, image *u);
+void convert_image_to_jpeg(const image *u, unsigned char* image_chars);
+void iso_diffusion_denoising(image *u, image *u_bar, float kappa, int iters);
 
 int main(int argc, char *argv[])
 {
@@ -30,15 +35,15 @@ int main(int argc, char *argv[])
   /* read from command line: kappa, iters, input_jpeg_filename, output_jpeg_filename */
   /* ... */
 	if (argc == 5) {
-		kappa = atoi(argv[1]);
+		kappa = atof(argv[1]);
 		iters = atoi(argv[2]);
 		input_jpeg_filename = argv[3];
 		output_jpeg_filename = argv[4];
-		printf("iters %d \n kappa %d \n input filename %s \n output filename %s \n", iters
+		printf("iters %d \n kappa %f \n input filename %s \n output filename %s \n", iters
 			, kappa, input_jpeg_filename, output_jpeg_filename);
 	}
 	else {
-		printf("Wrong usage");
+		printf("Usage: ./serial_main iters kappa input_filename output_filename\n");
 		exit(1);
 	}
 
@@ -52,4 +57,72 @@ int main(int argc, char *argv[])
 	deallocate_image (&u);
 	deallocate_image (&u_bar);
 	return 0; 
+}
+
+void allocate_image(image *u, int m, int n) {
+	int i;
+	u->m = m; u->n=n;
+	u->image_data = malloc(m*sizeof(double*));
+	for (i = 0; i<m; i++) {
+		u->image_data[i] = malloc(n*sizeof(double));
+	}
+}
+void deallocate_image(image *u){
+	int i;
+	for (i=0; i<u->m; i++) {
+		free(u->image_data[i]);
+	}
+}
+void convert_jpeg_to_image(const unsigned char* image_chars, image *u){
+	int m = u->m;
+	int n = u->n;
+	int i, j;
+	printf("Converting JPEG to image \n");
+	for (i = 0; i<m; i++){
+		for (j = 0; j<n; j++) {
+			u->image_data[i][j] = image_chars[i*n+j];
+		}
+	}
+}
+void convert_image_to_jpeg(const image *u, unsigned char* image_chars){
+	int m = u->m;
+	int n = u->n;
+	int i, j;
+	for (i = 0; i<m; i++) {
+		for (j=0; j<n; j++) {
+			image_chars[i*n+j] = u->image_data[i][j];
+		}
+	}
+}
+void iso_diffusion_denoising(image *u, image *u_bar, float kappa, int iters){
+	int i, j, k; // Loop variables
+	int m = u->m;
+	int n = u->n;
+	float** u_bar_array = u_bar->image_data;
+	float** u_array = u->image_data;
+	
+	for (k = 0; k<iters; k++) {
+		printf("Iteration number %d \n", k);
+		printf("m=%d n=%d \n", i, j);
+		for (i=1; i<m-1; i++) {
+			for (j=1; j<n-1; j++) {
+				u_bar_array[i][j] = u_array[i][j] + kappa*(u_array[i-1][j] + u_array[i][j-1] - 4*u_array[i][j] + u_array[i][j+1] + u_array[i+1]
+					[j]);
+			}
+		}
+		for (i = 0; i<m; i++) {
+			u_bar_array[i][0] = u_array[i][0];
+			u_bar_array[i][n-1] = u_array[i][n-1];
+		}
+		for (j = 0; j<n; j++) {
+			u_bar_array[0][j] = u_array[0][j];
+			u_bar_array[m-1][j] = u_array[m-1][j];
+		}
+
+		for (i=0; i<m; i++) {
+			for (j=0; j<n; j++) {
+				u_array[i][j] = u_bar_array[i][j];
+			}
+		}
+	}
 }
