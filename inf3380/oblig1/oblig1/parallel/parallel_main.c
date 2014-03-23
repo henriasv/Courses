0d03e3bb2279737b3704a98c10a7fa8e0c58b623
Henrik Sveinsson <henrik.sveinsson@me.com>
2014-03-23 14:26:19 +0100

    Started programming parallell code

--- inf3380/oblig1/oblig1/parallel/parallel_main.c
+++ inf3380/oblig1/oblig1/parallel/parallel_main.c
@@ -1,3 +1,27 @@
-int main () {
-  return 0;
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>  
+
+typedef struct
+{
+  float** image_data;  /* a 2D array of floats */
+  int m;               /* # pixels in x-direction */
+  int n;               /* # pixels in y-direction */
+} image;
+
+int main(int argc, char* argv[]) {
+	int m, n, c, iters;
+	int my_m, my_n, my_rank, num procs;
+	float kappa;
+	image u, u_bar, whole_image;
+	unsigned char* image_chars, my_image_chars;
+	char *input_jpeg_filename, *output_jpeg_filename;
+
+	MPI_Init (&argc, &argv);
+	MPI_Comm_Rank (MPI_COMM_WORLD);
+	MPI_Comm_Size (MPI_COMM_WORLD);
+
+
+
+	return 0;
 }
